e21c89f17e9602540fd19565ae853dd894732368
sidney2k1 <sidsingla02@gmail.com>
2025-01-04 11:14:05 +0530

    main2

---
+++ main2.py
@@ -0,0 +1,15 @@
+def seive(num):
+    prime=[True for i in range(num+1)]
+    p=2
+    while(p*p<=num):
+        if (prime[p]==True):
+            for i in range(p*p,num+1,p):
+                prime[i]=False
+        p+=1
+    for p in range(2,num+1):
+        if prime[p]:
+            print(p)
+num=int(input("Enter the number"))
+print("following are the prime numbers smaller")
+print("than or equal to",num)
+seive(num)
